4f098dd8bbd0522c5acf1eafb5f748cee9788aea
Vincent Juma <61782453+juma-vincent@users.noreply.github.com>
2020-07-05 22:05:24 +0300

    fix

--- controllers/register.js
+++ controllers/register.js
@@ -18,7 +18,8 @@ const handleRegister = (req, res, db, bcrypt) => {
       })
       .then(trx.commit)
       .catch(trx.rollback);
-  }).catch((err) => res.status(400).json("Unable to register"));
+  });
+  // .catch((err) => res.status(400).json("Unable to register"));
 };
 
 module.exports = { handleRegister: handleRegister };

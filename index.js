df58437d340891ad9093e7ff77aba85b7e45afc9
Vincent Juma <61782453+juma-vincent@users.noreply.github.com>
2020-07-05 10:03:44 +0300

    added database connection

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-polished-58771",
-    user: "postgres",
-    password: "2416Vince",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
